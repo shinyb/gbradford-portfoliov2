c653a91b84a8617bc507d2d5026df80b4e3a4015
shinyb <gloria_bradford2@yahoo.com>
2022-10-16 08:01:20 -0500

    duplicate portfolio files

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+document.getElementByTagName('a').addEventListener('mouseover', function(event) {
+    event.target.style.backgroundColor = "#ef9c70"
+    event.target.style.borderColor = "#827e7e"
+    event.target.style.cursor = "pointer"
+})
