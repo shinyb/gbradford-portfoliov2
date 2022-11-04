1e098dd721a1f830cf1b4bf727758e138ac375f4
shinyb <gloria_bradford2@yahoo.com>
2022-11-04 08:49:30 -0500

    copied portfolio redo files

--- index.js
+++
@@ -1,7 +0,0 @@
-
-
-document.getElementByTagName('a').addEventListener('mouseover', function(event) {
-    event.target.style.backgroundColor = "#ef9c70"
-    event.target.style.borderColor = "#827e7e"
-    event.target.style.cursor = "pointer"
-})
